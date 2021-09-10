eca7fb4835a16552b3789c09c0b9d4e981ddd8f0
sarahdelacruz <71873629+sarahdelacruz@users.noreply.github.com>
2021-09-10 14:59:46 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,19 +59,19 @@ ReactDOM.render(
         author="ANUSHA VELUSWAMY"
         />
         <Books
-        image="https://books.google.com.ph/books/publisher/content?id=IG3yCAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE72_avsdwRoMAlYwa3iPnXJwTTAwtEHODSjSr9Sj10D1LqGkXms4EffL_plIm_mV3SdpiFK8RdVBM0k104Zwh2sfQZqxAneYxjxcfzK0husf_-Qc6DE8uZYE2DZCGvjmNlBZO2KF"
+        image="https://books.google.com.ph/books/publisher/content?id=lcfDCgAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73cTPkfEksLYnNxmERuboXR7XOV0sTQLdDBA8MKDIG26ZBEUKqUBx1EIxmSa1WrfxT3T25B0SDME_wq11kr4EOqxAjN0GHKlyQLQoT7XCI2SGgIUFDZ346xmpjD_ENfEg4P1pGO"
         preview="https://books.google.com.ph/books?id=IG3yCAAAQBAJ&pg=PA371&dq=intitle:Structure+and+Space+CAROL+WINTER&hl=&as_pt=BOOKS&cd=2&source=gbs_api#v=onepage&q=intitle%3AStructure%20and%20Space%20CAROL%20WINTER&f=false"
         title="THE POPULATION STRUCTURE OF VEGETATION"
         author="J. WHITE"
         />
         <Books
-        image="https://books.google.com.ph/books/publisher/content?id=uUMQEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71iJ6Lk1vlZLctbK9uePsI_YXZTqcB2Tvlc13IIWVX8nkmek3CwX74tBwn5uJUVw6yNFxR1mG-ET4egd8mob1MZ-JK2MxV9fMA9sBYRCTtVMNOWOFMJfBI8oGc2gyhUHUy2pAfm"
+        image="https://books.google.com.ph/books/content?id=BX6GiV1OqGAC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE70cmaac2Duf5w9hrPrSArs-MEJxAYdx2LxHz7AVqGm4qNLRAo_lDisIkZMwLOWsdhrxT7yv7_R07bYH7k4bbq2JdXSju3qX_lbmcCs6Y9IFzixPUELq5rlBAviFdPOzWdpi4KHg"
         preview="https://books.google.com.ph/books?id=uUMQEAAAQBAJ&printsec=frontcover&dq=intitle:react+js&hl=&as_pt=BOOKS&cd=17&source=gbs_api#v=onepage&q=intitle%3Areact%20js&f=false"
         title="FULL-STACK REACT, TYPESCRIPT, AND NODE"
         author="DAVID CHOI"
         />
         <Books
-        image="https://books.google.com.ph/books/publisher/content?id=IVToDQAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE73apqhlKupri_zeX8nmKeWPjZ-pWPt2odLedSC4vyfuJTYR4AOp7UDNuZyUWaDPIpHv5W4cyIgnPqyHqo0PBnfN_zuGFmRxL8YfNjamM6bFKpcCf7HpD6HuiKiruiJhJ50Xp1EI"
+        image="https://books.google.com.ph/books/content?id=pZ5SzQEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70MudvtI8uTv2-3titeKYrcnW0ZAywHFWzokMdpiiEbkduGra4cNlHi6yh3-DvPYLlqsiw0cERu0BnTwfd07NVqbeuQWC99gEbK73hJEjMyta93mIbryeYqtiIFc6FISBYXuOaV"
         details=""
         preview="https://books.google.com.ph/books?id=IVToDQAAQBAJ&printsec=frontcover&dq=intitle:Life+of+a+worm&hl=&as_pt=BOOKS&cd=15&source=gbs_api#v=onepage&q=intitle%3ALife%20of%20a%20worm&f=false"
         title="A LIFE FOR GOD"
